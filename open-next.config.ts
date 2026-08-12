22505476405a3576093eb5be59c3b7230c77e788
Utkarsh-Mani-Tripathi-GIT <Utkarshmanitripathi2006@gmail.com>
2026-08-12 10:49:32 +0530

    Configure OpenNext build script to generate .open-next/worker.js for Cloudflare Pages

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,21 +1,24 @@
-const config = {
+export default {
   default: {
     override: {
       wrapper: 'cloudflare-node',
       converter: 'edge',
+      proxyExternalRequest: 'fetch',
       incrementalCache: 'dummy',
       tagCache: 'dummy',
       queue: 'dummy',
     },
   },
+  edgeExternals: ['node:crypto'],
   middleware: {
     external: true,
     override: {
-      wrapper: 'cloudflare-node',
+      wrapper: 'cloudflare-edge',
       converter: 'edge',
       proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'dummy',
     },
   },
 };
-
-export default config;
